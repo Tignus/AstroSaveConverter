fdc54fc4d0441e7023c6cf276d7d26ca4d87ddfb
WinXaito <github.soc@winxaito.com>
2020-07-29 16:48:55 +0200

    Base Window for GUI

---
+++ AstroSaveGui.py
@@ -0,0 +1,10 @@
+"""Minimalistic PySciter sample for Windows."""
+
+import sciter
+
+if __name__ == '__main__':
+    sciter.runtime_features(file_io=True, allow_sysinfo=True)
+
+    frame = sciter.Window(ismain=True, uni_theme=True)
+    frame.load_file("assets/gui/main.htm")
+    frame.run_app()
